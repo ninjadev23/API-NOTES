ce649d0e62d4a16ac6de4c6374afc4922bd4ef16
ninjadev23 <ninja48140@gmail.com>
2025-06-03 23:29:35 -0400

    add cors

--- index.ts
+++ index.ts
@@ -1,15 +1,23 @@
 import express from "express"
 import morgan from "morgan"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 import UserRouter from "./routes/users"
 import NoteRouter from "./routes/notes"
-import cookieParser from "cookie-parser"
 import dotenv from "dotenv"
+
 dotenv.config()
 import "./configDB"
+
 const app = express()
 app.use(morgan("dev"))
 app.use(express.json())
 app.use(cookieParser())
+
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 app.use("/api/users", UserRouter)
 app.use("/api/notes", NoteRouter)
 const PORT = process.env.PORT || 3000
