1e4cc2892bb7f429f85aabfa4d44062f02bc51e4
ninjadev23 <ninja48140@gmail.com>
2025-06-13 22:56:49 -0400

    Update index.ts

--- routes/users.ts
+++ routes/users.ts
@@ -1,76 +1,81 @@
 import { Router } from "express";
 import { UserModel } from "../models/Users";
 import { userSchema } from "../types";
-import {z} from "zod";
-import bcrypt from "bcryptjs"
-import jwt from "jsonwebtoken"
+import { z } from "zod";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { handleError } from "../utils";
 
-const router = Router()
+const router = Router();
 
- const cookiesConfig = {//Configuracion general de las cookies
-    maxAge: 1000 * 60 * 60 * 24 * 20, // 20 días
-    secure: process.env.NODE_ENV_FOR_SECURE === "production",
-    sameSite: "none" as const, //Sino se activa esto el frontend rechaza las cookies por estar en diferentes sitios
-                
-}
+const cookiesConfig = {
+  //Configuracion general de las cookies
+  maxAge: 1000 * 60 * 60 * 24 * 20, // 20 días
+  secure: process.env.NODE_ENV_FOR_SECURE === "production",
+  sameSite: "none" as const, //Sino se activa esto el frontend rechaza las cookies por estar en diferentes sitios
+};
 
-router.post("/signup",async (req, res)=>{
-    try{
-        // Validando entrada
-        const userValided = userSchema.parse(req.body)    
-        userValided.password = await bcrypt.hash(userValided.password, 10)
-        const newUser = new UserModel(userValided)
-        await newUser.save()
-        res.status(201).json({
-            message:"New User Created!"
-        })
-        
-    }catch(err){
-        handleError(res, (err as Error));
-    }
-})
-router.post("/login", async (req, res)=>{
-    try{
-        const userValidator = z.object({
-            email: z.string().email({message:"Incorrect Email"}),
-            password: z.string()
-        })
-        const userValided = userValidator.parse(req.body)
-        const userData = await UserModel.findOne({email: userValided.email})
-        if(!userData) throw new Error("User not found, please register first")    
-        if(await bcrypt.compare(userValided.password, userData.password)){
-            const token = jwt.sign({
-               userID: userData._id
-            },(process.env.SECRET_KEY as string),{
-                expiresIn: "20d" //fecha de expiracion del token 20 dias
-            })
-            res.cookie("name", userData.name, {...cookiesConfig})
-            res.cookie("access_token",token, {
-                httpOnly: true, //opcion que hace que esta cookie solo la pueda leer el servidor
-                ...cookiesConfig
-            }).json({
-                message: "User Authenticated Correctly"
-            })
-        }else{
-            throw new Error("Incorrect password")
-        }
-    }catch(err){
-        handleError(res, (err as Error));
-    }
-})
-router.delete("/logout", (_req, res) => {
-    try {
-        res.clearCookie("name", cookiesConfig);
-        res.clearCookie("access_token", {
-            httpOnly: true,
-            ...cookiesConfig
-        });
-        res.json({
-            message: "User Logged Out"
-        });
-    } catch (err) {
-        handleError(res, (err as Error));
-    }
+router.post("/signup", async (req, res) => {
+  try {
+    // Validando entrada
+    const userValided = userSchema.parse(req.body);
+    userValided.password = await bcrypt.hash(userValided.password, 10);
+    const newUser = new UserModel(userValided);
+    await newUser.save();
+    res.status(201).json({
+      message: "New User Created!",
+    });
+  } catch (err) {
+    handleError(res, err as Error);
+  }
 });
-export default router
+router.post("/login", async (req, res) => {
+  try {
+    const userValidator = z.object({
+      email: z.string().email({ message: "Incorrect Email" }),
+      password: z.string(),
+    });
+    const userValided = userValidator.parse(req.body);
+    const userData = await UserModel.findOne({ email: userValided.email });
+    if (!userData) throw new Error("User not found, please register first");
+    if (await bcrypt.compare(userValided.password, userData.password)) {
+      const token = jwt.sign(
+        {
+          userID: userData._id,
+        },
+        process.env.SECRET_KEY as string,
+        {
+          expiresIn: "20d", //fecha de expiracion del token 20 dias
+        }
+      );
+      res.cookie("name", userData.name, { httpOnly: false, ...cookiesConfig });
+      res
+        .cookie("access_token", token, {
+          httpOnly: true, //opcion que hace que esta cookie solo la pueda leer el servidor
+          ...cookiesConfig,
+        })
+        .json({
+          message: "User Authenticated Correctly",
+        });
+    } else {
+      throw new Error("Incorrect password");
+    }
+  } catch (err) {
+    handleError(res, err as Error);
+  }
+});
+router.delete("/logout", (_req, res) => {
+  try {
+    res.clearCookie("name", cookiesConfig);
+    res.clearCookie("access_token", {
+      httpOnly: true,
+      ...cookiesConfig,
+    });
+    res.json({
+      message: "User Logged Out",
+    });
+  } catch (err) {
+    handleError(res, err as Error);
+  }
+});
+export default router;
