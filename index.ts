18e10600c3af7e7bd61c071c1f11bb3b541f3883
ninjadev23 <ninja48140@gmail.com>
2025-06-15 14:17:04 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://notes-typescript-react-app.netlify.app/"],
+  origin: ["http://localhost:5173", "https://notes-typescript-react-app.netlify.app"],
   credentials: true
 }));
 app.use("/api", UserRouter)
